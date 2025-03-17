147a082d31a1c4ee35f4b2a0aff2969874fb5159
Oscar Flores <l19251031@sjuanrio.tecnm.mx>
2025-03-17 14:14:54 -0600

    Actualiza y elimina posts

--- app/routes/post.py
+++ app/routes/post.py
@@ -28,3 +28,26 @@ def add_posts():
         #Aqui sigue si es GET
     categories = Category.query.all()
     return render_template('posts/create_post.html', categories=categories)
+
+#Actualizar post
+@posts_bp.route('/posts/update/<int:id>', methods=['GET','POST'])
+def update_posts(id):
+    post = Post.query.get(id)
+    if request.method == 'POST':
+        post.title = request.form['title']
+        post.category_id = request.form['category_id']
+        post.content = request.form['content']
+        db.session.commit()
+        return redirect(url_for('posts.listar_posts'))
+    
+    categories = Category.query.all()
+    return render_template('posts/update_post.html', post=post, categories=categories)
+
+#Eliminar post
+@posts_bp.route('/posts/delete/<int:id>')
+def delete_posts(id):
+    post = Post.query.get(id)
+    if post:
+        db.session.delete(post)
+        db.session.commit()
+    return redirect(url_for('posts.listar_posts'))
