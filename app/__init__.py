dae752d8563783319180c13533f3a06af56c3e4c
Oscar Flores <l19251031@sjuanrio.tecnm.mx>
2025-03-17 14:09:16 -0600

    Muestra e inserta posts

---
+++ app/__init__.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv 
+
+# Cargar las variables de entorno
+load_dotenv()
+
+# Crear la instancia de la aplicación Flask
+app = Flask(__name__)
+
+# Configuración de la base de datos PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializar la base de datos
+db = SQLAlchemy(app)
+
+# Importar modelos para SQLAlchemy los reconozca
+from app.models import Post, Category
+
+# Importar y registrar blueprints
+from app.routes.post import posts_bp
+
+
+# Registrar blueprints
+app.register_blueprint(posts_bp, url_prefix='/posts')
+
+
+# Ruta principal
+@app.route('/')
+def index():
+    return render_template('home.html')
